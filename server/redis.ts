54719773bc5050d49bad062c98d715b44e73cde3
datta-sayak <174126344+datta-sayak@users.noreply.github.com>
2025-12-10 21:14:57 +0530

    fix: added redis URL param

--- server/redis.ts
+++ server/redis.ts
@@ -1,8 +1,14 @@
 import { createClient } from "redis";
+import dotenv from "dotenv"
+dotenv.config()
 
-export const redis = createClient();
+export const redis = createClient({
+    url: process.env.REDIS_URL!
+});
 
-export const redisSubscriber = createClient();
+export const redisSubscriber = createClient({
+    url: process.env.REDIS_URL!
+});
 redis.on("error", err => console.error("Redis Client Error", err));
 
 await redis.connect();
