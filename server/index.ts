2fb7d43ec2a1686b83de63e3a76db0186f6fe083
Sayak Datta <174126344+datta-sayak@users.noreply.github.com>
2025-12-24 05:25:37 +0530

    fix: env

--- server/index.ts
+++ server/index.ts
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT || 4000, () => {
 
 export const io = new Server(server, {
     cors: {
-        origin: process.env.NODE_ENV || "http://localhost:3000",
+        origin: process.env.NEXT_CLIENT || "http://localhost:3000",
     },
 });
 
